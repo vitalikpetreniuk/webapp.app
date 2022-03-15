d0c1e042a6835c49815fb8c1d381e060a77c3c69
vitaliy <petreniuk.ua@gmail.com>
2022-03-15 10:48:18 +0200

    update 15.03

---
+++ resources/views/components/reportings-top.blade.php
@@ -0,0 +1,11 @@
+<x-reporting-analytics-tabs/>
+<div class="tabs-section active mt-12">
+    <button class="active">daily</button>
+    <button>monthly</button>
+</div>
+<div class="datepicker d-flex mt-20">
+    <input id="datepicker" type="text" class="flatpickr-input" readonly="readonly">
+    <div class="datepicker__icon d-flex align-items-center justify-content-center">
+        <img src="{{ asset('frontend/images/dist/icons/calendar.svg') }}" alt="">
+    </div>
+</div>
