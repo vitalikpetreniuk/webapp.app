9d0a92df79538e1e09172ac2392537f833ae5a3c
vitaliy <petreniuk.ua@gmail.com>
2022-03-21 12:04:06 +0200

    update 15.03

---
+++ app/Http/Controllers/SourceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class SourceController extends Controller
+{
+    public function listSources() {
+        $user_id = Auth::id() ?? $_GET['user_id'];
+        $select = DB::select("SELECT name, id FROM sources WHERE user_id =?", [$user_id]);
+        $data = [];
+        foreach ($select as $item) {
+            $data[$item->id] = $item->name;
+        }
+        return '['.json_encode($data).']';
+    }
+}
