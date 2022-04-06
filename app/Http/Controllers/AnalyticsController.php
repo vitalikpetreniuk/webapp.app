0ba05e2448aa7b618520ddb08509e13fa5f3e4eb
vitaliy <petreniuk.ua@gmail.com>
2022-04-06 10:57:41 +0300

    change AnalyticsController.php display date

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -16,8 +16,8 @@ class AnalyticsController extends Controller
     {
 //        $from = Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d');
 //        $to = Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d');
-        $from = '2021-01-01';
-        $to = '2022-03-31';
+        $from = '1990-01-01';
+        $to = '2022-04-31';
         $expenses = ExpenseController::getAllExpenses($from, $to);
         $revenues = RevenueController::getAllRevenues($from, $to);
         return view('reportings/reportings', ['data' => $this->prepareAnalyticsData($from, $to)]);
