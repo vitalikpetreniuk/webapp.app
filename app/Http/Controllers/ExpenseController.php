142d83068048d93a359bc6ee5b563e6c04860413
vitaliy <petreniuk.ua@gmail.com>
2022-04-06 10:33:33 +0300

    debug

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -111,9 +111,6 @@ class ExpenseController extends Controller
             return false;
         }
 
-        var_dump($stringdate);
-        var_dump($sep);
-
         return Carbon::createFromFormat("n" . $sep . "j" . $sep . "Y", $stringdate);
     }
 
