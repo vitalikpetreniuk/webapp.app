c7b32e4e26d21d3157dc6b97889ea0422bd4dbc6
vitaliy <petreniuk.ua@gmail.com>
2022-04-08 15:11:05 +0300

    update

--- resources/views/components/chart-pl.blade.php
+++ resources/views/components/chart-pl.blade.php
@@ -1,3 +1,8 @@
+<form action="" method="get">
+    <input type="text" name="from">
+    <input type="text" name="to">
+    <input type="submit" value="Поиск">
+</form>
 <div id="chartdiv" class="mt-20"></div>
 @if(isset($chart_data))
     <script>
