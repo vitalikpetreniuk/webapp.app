d81023e12b4ea0119a65b2b2ce9163457b28927c
vitaliy <petreniuk.ua@gmail.com>
2022-09-30 10:38:45 +0300

    add bc file

---
+++ bc.js
@@ -0,0 +1,19 @@
+<!-- Global site tag (gtag.js) - Google Ads: AW-939789346 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-939789346"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-939789346');
+</script>
+
+<!-- Event snippet for Purchase Big Commerce conversion page -->
+<script>
+    gtag('event', 'conversion', {
+    'send_to': 'AW-939789346/q39KCJ61n-IDEKKYkMAD',
+    'value': %%GLOBAL_OrderAmount%%,
+    'currency': 'USD',
+    'transaction_id': ''%%GLOBAL_OrderId%%'
+});
+</script>
